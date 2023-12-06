552ce40936ecf793ecc7fb2c2b25fc005e27a3ea
Ninuskaninus <89078328+Ninuskaninus@users.noreply.github.com>
2023-12-06 14:37:59 +0100

    setup workflow

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+module.exports = {
+  entry: './index.html', // Adjust the entry file based on your project
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    contentBase: './dist',
+    hot: true,
+  },
+};
